1f6edc1b6ab39402e5b86ffa390d98667b377cfb
Danielle B <Danielle B>
2017-03-23 19:49:35 -0500

    init

---
+++ App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var x = new HttpClient();
+            var y = x.GetStringAsync("http://localhost:60519/api/values").Result;
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BattleNet/ILiquid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleNet
+{
+    interface ILiquid
+    {
+        void Flow();
+    }
+
+    public class Liquid : ILiquid
+    {
+        public void Flow()
+        {
+
+        }
+    }
+}
